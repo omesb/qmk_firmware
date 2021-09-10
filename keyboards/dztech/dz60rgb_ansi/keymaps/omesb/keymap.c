518890189e19f6134618bfc74885b8b64ec6b717
omesb <omesgit@gmail.com>
2021-09-10 16:13:55 +0200

    keymap with arrow keys

--- keyboards/dztech/dz60rgb_ansi/keymaps/omesb/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/omesb/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
         KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,                      RSFT_T(KC_RSFT),
-        TT(1), KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_LGUI, KC_APP, KC_RCTL
+        TT(1), KC_LGUI, KC_LALT, KC_SPC, KC_RALT, TO(0), TO(2), TO(3)
     ),
 
     /** FN Layer */
@@ -30,12 +30,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TT(1), _______, _______, _______, _______, _______, _______, RESET
     ),
 
-    // /** NORMAL ANSI LAYER (GAMES) */
-    // [3] = LAYOUT_60_ansi(
-    //     KC_GESC,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_DEL,
-    //     KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-    //     KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,
-    //     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT, KC_SLSH,                   KC_RSFT,
-    //     MO(1), KC_LGUI, KC_LALT, KC_SPC, OSM(MOD_RALT), _______, _______, _______
-    // ),
+    /** NORMAL ANSI LAYER (GAMES) */
+    [3] = LAYOUT_60_ansi(
+        KC_GESC,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_DEL,
+        KC_TAB,           KC_Q,    KC_UP,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
+        KC_LCTL,          KC_LEFT,    KC_DOWN,    KC_RIGHT,    KC_F,    KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,                      RSFT_T(KC_RSFT),
+        TT(1), KC_LGUI, KC_LALT, KC_SPC, KC_RALT, TO(0), TO(2), TO(3)
+    ),
 };
